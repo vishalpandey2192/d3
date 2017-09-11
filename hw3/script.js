7225a05cd623146ce771e89673699dbd3dde888f
vishalpandey <vishal.pandey@utah.edu>
2017-09-10 20:17:15 -0600

    HW3

---
+++ hw3/script.js
@@ -0,0 +1,113 @@
+/**
+ * Makes the first bar chart appear as a staircase.
+ *
+ * Note: use only the DOM API, not D3!
+ */
+function staircase() {
+    // ****** TODO: PART II ******
+}
+
+/**
+ * Render the visualizations
+ * @param error
+ * @param data
+ */
+function update(error, data) {
+    if (error !== null) {
+        alert('Could not load the dataset!');
+    } else {
+        // D3 loads all CSV data as strings;
+        // while Javascript is pretty smart
+        // about interpreting strings as
+        // numbers when you do things like
+        // multiplication, it will still
+        // treat them as strings where it makes
+        // sense (e.g. adding strings will
+        // concatenate them, not add the values
+        // together, or comparing strings
+        // will do string comparison, not
+        // numeric comparison).
+
+        // We need to explicitly convert values
+        // to numbers so that comparisons work
+        // when we call d3.max()
+
+        for (let d of data) {
+            d.a = +d.a;
+            d.b = +d.b;
+        }
+    }
+
+    // Set up the scales
+    let aScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.a)])
+        .range([0, 150]);
+    let bScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.b)])
+        .range([0, 150]);
+    let iScale = d3.scaleLinear()
+        .domain([0, data.length])
+        .range([0, 110]);
+
+
+    // ****** TODO: PART III (you will also edit in PART V) ******
+
+    // TODO: Select and update the 'a' bar chart bars
+
+    // TODO: Select and update the 'b' bar chart bars
+
+    // TODO: Select and update the 'a' line chart path using this line generator
+
+    let aLineGenerator = d3.line()
+        .x((d, i) => iScale(i))
+        .y((d) => aScale(d.a));
+
+    // TODO: Select and update the 'b' line chart path (create your own generator)
+
+    // TODO: Select and update the 'a' area chart path using this area generator
+    let aAreaGenerator = d3.area()
+        .x((d, i) => iScale(i))
+        .y0(0)
+        .y1(d => aScale(d.a));
+
+    // TODO: Select and update the 'b' area chart path (create your own generator)
+
+    // TODO: Select and update the scatterplot points
+
+    // ****** TODO: PART IV ******
+
+}
+
+/**
+ * Load the file indicated by the select menu
+ */
+function changeData() {
+    let dataFile = document.getElementById('dataset').value;
+    if (document.getElementById('random').checked) {
+        randomSubset();
+    }
+    else {
+        d3.csv('data/' + dataFile + '.csv', update);
+    }
+}
+
+/**
+ *   Load the file indicated by the select menu, and then slice out a random chunk before passing the data to update()
+ */
+function randomSubset() {
+    let dataFile = document.getElementById('dataset').value;
+    if (document.getElementById('random').checked) {
+        d3.csv('data/' + dataFile + '.csv', function (error, data) {
+            let subset = [];
+            for (let d of data) {
+                if (Math.random() > 0.5) {
+                    subset.push(d);
+                }
+            }
+            update(error, subset);
+        });
+    }
+    else {
+        changeData();
+    }
+}
