ef428b3a9a9d94d3b0c83a38c1ceb7df2f6d6090
vishalpandey <vishal.pandey@utah.edu>
2017-09-10 20:11:51 -0600

    File

--- hw2/js/Tree.js
+++ hw2/js/Tree.js
@@ -11,126 +11,29 @@ class Tree {
 		//console.log(json);
 
 		var arr = json;
-		var flatArray = json;
-		var node_list = [];
+		this.node_list = [];
 
-
+// code to make a node tree
         for (var i = 0, len = arr.length; i < len; ++i) {
         	let node = new Node(arr[i].name,arr[i].parent);
-        	node_list.push(node);
+        	this.node_list.push(node);
 
 		}
 
-        for (var i = 0, len = node_list.length; i < len; ++i) {
-        	switch(node_list[i].name){
-				case 'Animal':
-					node_list[i].position = 0;
-					break;
-                case 'Reptile':
-                    node_list[i].position = 0;
-                    break;
-                case 'Mammal':
-                    node_list[i].position = 1;
-                    break;
-                case 'Lizard':
-                    node_list[i].position = 0;
-                    break;
-                case 'Snake':
-                    node_list[i].position = 1;
-                    break;
-                case 'Bird':
-                    node_list[i].position = 2;
-                    break;
-                case 'Equine':
-                    node_list[i].position = 3;
-                    break;
-                case 'Bovine':
-                    node_list[i].position = 4;
-                    break;
-                case 'Canine':
-                    node_list[i].position = 5;
-                    break;
-                case 'Canary':
-                    node_list[i].position = 2;
-                    break;
-                case 'Horse':
-                    node_list[i].position = 3;
-                    break;
-                case 'Zebra':
-                    node_list[i].position = 4;
-                    break;
-                case 'Cow':
-                    node_list[i].position = 5;
-                    break;
-                case 'Dog':
-                    node_list[i].position = 6;
-                    break;
-				default:
-                    node_list[i].position = 0;
 
-			}
-		}
-
-        for (var i = 0, len = node_list.length; i < len; ++i) {
-            for (var j = 0, len = node_list.length; j < len; ++j) {
+//code to add children to the nodes
+        for (var i = 0, len = this.node_list.length; i < len; ++i) {
+            for (var j = 0, len = this.node_list.length; j < len; ++j) {
             	//console.log(node_list[j].parentName);
-            	if(node_list[j].parentName == node_list[i].name){
-            		node_list[j].parentNode = node_list[i];
-            		node_list[i].children.push(node_list[j]);
+            	if(this.node_list[j].parentName == this.node_list[i].name){
+                    this.node_list[j].parentNode = this.node_list[i];
+                    this.node_list[i].children.push(this.node_list[j]);
 				}
 
 			}
 
 		}
 
-        function findDepth (data,depth) {
-            data.level = depth;
-            if(data.children != null){
-                for (var i = 0, len = data.children.length; i < len; ++i) {
-                    findDepth(data.children[i],depth+1)
-                }
-            }
-            return data;
-        }
-
-        // function findPosition (data,position) {
-        //     data.position = position;
-        //     if(data.children != null){
-        //         for (var i = 0, len = data.children.length; i < len; ++i) {
-        //         	data.children.position = i;
-        //             findPosition(data.children[i],)
-        //         }
-        //     }
-        //     return data;
-        // }
-        //
-        // function findHeight (data) {
-        // 	temp_array = [];
-        //     for (var i = 0, len = data.length; i < len; ++i) {
-        //     	if(data.level == i){
-        //
-			// 	}
-			// }
-        //
-        // }
-
-
-		var final_array = findDepth(node_list[0],0);
-        // var output_array = findPosition(final_array,0)
-
-		console.log(final_array);
-
-
-        // for (var i = 0, len = node_list[0].length; i < len; ++i) {
-        	// node_list[i].level = i;
-        	//
-        //
-		// }
-
-
-
-        this.renderTree(final_array)
-		
 	}
 
 
@@ -141,49 +44,130 @@ class Tree {
 	buildTree() {
 
 	//Assign Positions and Levels by making calls to assignPosition() and assignLevel()
+
+		this.assignLevel(this.node_list[0],0);
+        this.assignPosition(this.node_list[0],0);
+
+		console.log(this.node_list)
 	}
 
 	/**
 	 * Recursive function that assign positions to each node
 	 */
+
 	assignPosition(node, position) {
-		
-	}
+
+		//to check if parent position greater than position then add parent position to the position
+
+		if(node.parentNode != null && node.parentNode.position > position) position = node.parentNode.position
+
+
+		// to check for nodes at same level and find the highest value of position if present, increment it by 1 and assign to position of this node
+
+		for(var i = 0; i<this.node_list.length;i++) {
+			if(this.node_list[i].level == node.level){
+				if(this.node_list[i].position >= position) position = this.node_list[i].position+1;
+			}
+		}
+
+		node.position = position;
+
+		// explore all the children of the node recursively
+
+        // let child_nodes = node.children;
+        // child_nodes.forEach(function(node_child) {
+        //     this.assignPosition(node_child,0);
+        // });
+
+		for(var i=0,len =node.children.length; i<len;++i) this.assignPosition(node.children[i],0);
+
+    }
+
 
 	/**
 	 * Recursive function that assign levels to each node
 	 */
 	assignLevel(node, level) {
+		//console.log(node,level);
+        node.level = level;
+        if(node.children != null){
+            for (var i = 0, len = node.children.length; i < len; ++i) {
+                this.assignLevel(node.children[i],level+1);
+
+            }
+        }
+        return node;
 		
 	}
 
 	/**
 	 * Function that renders the tree
 	 */
-	renderTree(final_array) {
-        // Select the 'svg' element from the DOM wth d3
-        let svg = d3.select("svg");
+	renderTree() {
 
+        let g = d3.select("g").data(this.node_list);
 
-        // Select all circles using selectAll()
-        // bind set1 to the selection using data()
-        var circles = svg.selectAll("circle")
-            .data(final_array.children);
-        var newData=circles.enter().append("circle"); //append only to enter selection
-        circles.exit().remove();
+        var connectors = g.selectAll("line")
+			              .data(this.node_list);
+        var newconnectors=connectors.enter()
+									.append("line")
+									.attr("class", "line");
+
+        connectors=newconnectors.merge(connectors);
+
+        connectors.attr("x1", function (d) {
+            return d.level*125;
+        })
+            .attr("y1", function (d) {
+                return d.position*125;
+            })
+            .attr("x2", function (d) {
+            	if(d.parentNode!= null){
+                    return d.parentNode.level*125;
+				}
+
+            })
+            .attr("y2", function (d) {
+                if(d.parentNode!= null){
+                    return d.parentNode.position*125;
+				}
+
+            });
+
+        var circles = g.selectAll("circle")
+			           .data(this.node_list);
+        var newData=circles.enter()
+						   .append("circle");
+
         circles=newData.merge(circles);
 
         circles.attr("cx", function (d) {
-            return d.level*100;
+            return d.level*125;
         })
             .attr("cy", function (d) {
-                return d.position*100;
+                return d.position*125;
             })
             .attr("r",function(d){
-                return 10;
-            });
+                return 45;
+            })
+			.attr("class","circle");
 
-        console.log(circles);
+        var label = g.selectAll("g")
+            .data(this.node_list)
+            .enter();
+
+        label.append("text")
+            .attr("dx",
+            function(d) {
+                return d.level * 125;
+            }).attr("dy",
+            function(d) {
+                return d.position * 125;
+            })
+            .text(function(d) {
+                return d.name.toUpperCase();
+            })
+			.attr("class", "label");
 	}
 		
 }
